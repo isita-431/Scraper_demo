71a3533bb647792719b63837205ca628cc507796
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2022-12-22 16:46:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,47 +7,47 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# def Driver():
-#     @st.experimental_singleton
-#     def get_driver():
-#         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-#     options = Options()
-#     options.add_argument('--disable-gpu')
-#     options.add_argument('--headless')
-#     driver = get_driver()
-#     return driver
-
-# driver = Driver()
-# driver.get("https://www.lybrate.com/")
-# st.code(driver.page_source)
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://www.lybrate.com/"
-TIMEOUT = 20
+def Driver():
+#     @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    driver = get_driver()
+    return driver
 
-st.title("Test Selenium")
-
-firefoxOptions = Options()
-firefoxOptions.add_argument("--headless")
-service = Service(GeckoDriverManager().install())
-driver = webdriver.Firefox(
-    options=firefoxOptions,
-    service=service,
-)
-driver.get(URL)
+driver = Driver()
+driver.get("https://www.lybrate.com/")
 st.code(driver.page_source)
+# from selenium import webdriver
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from webdriver_manager.firefox import GeckoDriverManager
+
+# URL = "https://www.lybrate.com/"
+# TIMEOUT = 20
+
+# st.title("Test Selenium")
+
+# firefoxOptions = Options()
+# firefoxOptions.add_argument("--headless")
+# service = Service(GeckoDriverManager().install())
+# driver = webdriver.Firefox(
+#     options=firefoxOptions,
+#     service=service,
+# )
+# driver.get(URL)
+# st.code(driver.page_source)
 
 
 
