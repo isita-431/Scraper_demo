cbd84b2aebcbc0f2eb3e7d7b613d6c6046793c2d
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2022-12-22 16:28:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+def Driver():
+    @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    driver = get_driver()
+    return driver
 
-driver = get_driver()
+driver = Driver()
 driver.get("https://www.lybrate.com/")
 st.code(driver.page_source)
     
